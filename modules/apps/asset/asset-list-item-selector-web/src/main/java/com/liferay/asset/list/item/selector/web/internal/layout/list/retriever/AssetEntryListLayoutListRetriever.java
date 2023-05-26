d96b76293d98bbb9ab4ed858b5dfd862d554e8df
mikellorza <mikel.lorza@liferay.com>
2023-05-26 15:50:20 +0200

    Test sign 1

--- modules/apps/asset/asset-list-item-selector-web/src/main/java/com/liferay/asset/list/item/selector/web/internal/layout/list/retriever/AssetEntryListLayoutListRetriever.java
+++ modules/apps/asset/asset-list-item-selector-web/src/main/java/com/liferay/asset/list/item/selector/web/internal/layout/list/retriever/AssetEntryListLayoutListRetriever.java
@@ -102,7 +102,7 @@ public class AssetEntryListLayoutListRetriever
 		AssetListEntry assetListEntry =
 			_assetListEntryLocalService.fetchAssetListEntry(
 				classedModelListObjectReference.getClassPK());
-		System.out.println("hey");
+		System.out.println("hey1");
 		if (assetListEntry == null) {
 			return 0;
 		}
