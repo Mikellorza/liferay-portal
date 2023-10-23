df342c842d4c9955f14fdd1a994cc78f9ef30fe1
mikellorza <mikel.lorza@liferay.com>
2023-10-23 18:45:52 +0200

    LPS-199515 Filter journal articles by mine

--- modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/display/context/JournalDisplayContext.java
+++ modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/display/context/JournalDisplayContext.java
@@ -1366,7 +1366,7 @@ public class JournalDisplayContext {
 	}
 
 	private BooleanClause<Query>[] _getBooleanClauses(
-		long[] assetCategoryIds, String[] assetTagNames) {
+		long[] assetCategoryIds, String[] assetTagNames, long userId) {
 
 		BooleanQuery booleanQuery = new BooleanQueryImpl();
 
@@ -1382,6 +1382,11 @@ public class JournalDisplayContext {
 			booleanFilter.add(
 				_getAssetTagNamesFilter(assetTagNames),
 				BooleanClauseOccur.MUST);
+		}
+
+		if (userId > 0) {
+			booleanFilter.addTerm(
+				Field.USER_ID, String.valueOf(userId), BooleanClauseOccur.MUST);
 		}
 
 		booleanQuery.setPreBooleanFilter(booleanFilter);
@@ -1630,8 +1635,15 @@ public class JournalDisplayContext {
 
 		searchContext.setAttributes(attributes);
 
+		long userId = 0;
+
+		if (isNavigationMine()) {
+			userId = _themeDisplay.getUserId();
+		}
+
 		searchContext.setBooleanClauses(
-			_getBooleanClauses(_getAssetCategoryIds(), _getAssetTagNames()));
+			_getBooleanClauses(
+				_getAssetCategoryIds(), _getAssetTagNames(), userId));
 
 		long ddmStructureId = ParamUtil.getLong(
 			_httpServletRequest, "ddmStructureId");
