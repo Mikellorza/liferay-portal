1ad67e45a9327e960aa411191a4d81a8f57bb83f
mikellorza <mikel.lorza@liferay.com>
2023-10-23 18:41:37 +0200

    LPS-199515 Filter journal articles by asset category ids

--- modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/display/context/JournalDisplayContext.java
+++ modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/display/context/JournalDisplayContext.java
@@ -1116,7 +1116,8 @@ public class JournalDisplayContext {
 
 	public boolean isSearch() {
 		if (Validator.isNotNull(getKeywords()) ||
-			ArrayUtil.isNotEmpty(_getAssetTagNames())) {
+			ArrayUtil.isNotEmpty(_getAssetTagNames()) ||
+			ArrayUtil.isNotEmpty(_getAssetCategoryIds())) {
 
 			return true;
 		}
@@ -1312,6 +1313,31 @@ public class JournalDisplayContext {
 		return _articleSearchContainer;
 	}
 
+	private long[] _getAssetCategoryIds() {
+		if (!FeatureFlagManagerUtil.isEnabled("LPS-196766")) {
+			return null;
+		}
+
+		if (_assetCategoryIds == null) {
+			_assetCategoryIds = ParamUtil.getLongValues(
+				_httpServletRequest, "assetCategoryId");
+		}
+
+		return _assetCategoryIds;
+	}
+
+	private Filter _getAssetCategoryIdsFilter(long[] assetCategoryIds) {
+		BooleanFilter booleanFilter = new BooleanFilter();
+
+		for (long assetCategoryId : assetCategoryIds) {
+			booleanFilter.addTerm(
+				Field.ASSET_CATEGORY_IDS, String.valueOf(assetCategoryId),
+				BooleanClauseOccur.MUST);
+		}
+
+		return booleanFilter;
+	}
+
 	private String[] _getAssetTagNames() {
 		if (!FeatureFlagManagerUtil.isEnabled("LPS-196766")) {
 			return null;
@@ -1328,10 +1354,6 @@ public class JournalDisplayContext {
 	}
 
 	private Filter _getAssetTagNamesFilter(String[] assetTagNames) {
-		if (ArrayUtil.isEmpty(assetTagNames)) {
-			return null;
-		}
-
 		BooleanFilter booleanFilter = new BooleanFilter();
 
 		for (String assetTagName : assetTagNames) {
@@ -1343,10 +1365,18 @@ public class JournalDisplayContext {
 		return booleanFilter;
 	}
 
-	private BooleanClause<Query>[] _getBooleanClauses(String[] assetTagNames) {
+	private BooleanClause<Query>[] _getBooleanClauses(
+		long[] assetCategoryIds, String[] assetTagNames) {
+
 		BooleanQuery booleanQuery = new BooleanQueryImpl();
 
 		BooleanFilter booleanFilter = new BooleanFilter();
+
+		if (ArrayUtil.isNotEmpty(assetCategoryIds)) {
+			booleanFilter.add(
+				_getAssetCategoryIdsFilter(assetCategoryIds),
+				BooleanClauseOccur.MUST);
+		}
 
 		if (ArrayUtil.isNotEmpty(assetTagNames)) {
 			booleanFilter.add(
@@ -1601,7 +1631,7 @@ public class JournalDisplayContext {
 		searchContext.setAttributes(attributes);
 
 		searchContext.setBooleanClauses(
-			_getBooleanClauses(_getAssetTagNames()));
+			_getBooleanClauses(_getAssetCategoryIds(), _getAssetTagNames()));
 
 		long ddmStructureId = ParamUtil.getLong(
 			_httpServletRequest, "ddmStructureId");
@@ -1639,6 +1669,7 @@ public class JournalDisplayContext {
 	private JournalArticleDisplay _articleDisplay;
 	private SearchContainer<?> _articleSearchContainer;
 	private SearchContainer<JournalArticle> _articleVersionsSearchContainer;
+	private long[] _assetCategoryIds;
 	private final AssetDisplayPageFriendlyURLProvider
 		_assetDisplayPageFriendlyURLProvider;
 	private String[] _assetTagNames;
