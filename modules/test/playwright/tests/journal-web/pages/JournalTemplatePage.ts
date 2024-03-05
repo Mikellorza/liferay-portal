162f09f29a6673f71ef7eb7ac88568652ed36076
mikellorza <mikel.lorza@liferay.com>
2024-03-05 11:29:14 +0100

    LPD-19464 Add journal template page to manage templates

---
+++ modules/test/playwright/tests/journal-web/pages/JournalTemplatePage.ts
@@ -0,0 +1,31 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+import {Locator, Page} from '@playwright/test';
+
+import {JournalPage} from './JournalPage';
+
+export class JournalTemplatePage {
+	readonly page: Page;
+
+	readonly journalPage: JournalPage;
+	readonly newButton: Locator;
+
+	constructor(page: Page) {
+		this.page = page;
+
+		this.newButton = page.getByText('New', {exact: true});
+	}
+
+	async goto() {
+		await this.journalPage.goToTemplates();
+	}
+
+	async goToCreateNewTemplate() {
+		await this.newButton.waitFor();
+
+		await this.newButton.click();
+	}
+}
