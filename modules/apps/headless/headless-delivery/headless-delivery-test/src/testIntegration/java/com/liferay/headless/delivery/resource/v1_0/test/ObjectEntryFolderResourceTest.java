0117d2d498e49186dd2e690f4d28e2fe3b030789
Alicia García <alicia.garcia@liferay.com>
2025-02-18 11:18:26 +0100

    LPD-48867 fix test

--- modules/apps/headless/headless-delivery/headless-delivery-test/src/testIntegration/java/com/liferay/headless/delivery/resource/v1_0/test/ObjectEntryFolderResourceTest.java
+++ modules/apps/headless/headless-delivery/headless-delivery-test/src/testIntegration/java/com/liferay/headless/delivery/resource/v1_0/test/ObjectEntryFolderResourceTest.java
@@ -6,6 +6,9 @@
 package com.liferay.headless.delivery.resource.v1_0.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.headless.delivery.client.dto.v1_0.ObjectEntryFolder;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.test.rule.FeatureFlags;
 
 import org.junit.runner.RunWith;
@@ -17,4 +20,48 @@ import org.junit.runner.RunWith;
 @RunWith(Arquillian.class)
 public class ObjectEntryFolderResourceTest
 	extends BaseObjectEntryFolderResourceTestCase {
+
+	@Override
+	protected String[] getAdditionalAssertFieldNames() {
+		return new String[] {"label", "name"};
+	}
+
+	@Override
+	protected ObjectEntryFolder randomObjectEntryFolder() throws Exception {
+		return new ObjectEntryFolder() {
+			{
+				assetLibraryId = testDepotEntry.getDepotEntryId();
+				assetLibraryKey = testGroup.getGroupKey();
+				dateCreated = RandomTestUtil.nextDate();
+				dateModified = RandomTestUtil.nextDate();
+				externalReferenceCode = StringUtil.toLowerCase(
+					RandomTestUtil.randomString());
+				id = RandomTestUtil.randomLong();
+				label = StringUtil.toLowerCase(RandomTestUtil.randomString());
+				name = StringUtil.toLowerCase(RandomTestUtil.randomString());
+				numberOfObjectEntries = RandomTestUtil.randomInt();
+				numberOfObjectEntryFolders = RandomTestUtil.randomInt();
+				parentObjectEntryFolderId = 0L;
+			}
+		};
+	}
+
+	@Override
+	protected ObjectEntryFolder
+			testGraphQLObjectEntryFolder_addObjectEntryFolder()
+		throws Exception {
+
+		return objectEntryFolderResource.postAssetLibraryObjectEntryFolder(
+			testDepotEntry.getDepotEntryId(), randomObjectEntryFolder());
+	}
+
+	@Override
+	protected ObjectEntryFolder
+			testPatchObjectEntryFolder_addObjectEntryFolder()
+		throws Exception {
+
+		return objectEntryFolderResource.postAssetLibraryObjectEntryFolder(
+			testDepotEntry.getDepotEntryId(), randomObjectEntryFolder());
+	}
+
 }
