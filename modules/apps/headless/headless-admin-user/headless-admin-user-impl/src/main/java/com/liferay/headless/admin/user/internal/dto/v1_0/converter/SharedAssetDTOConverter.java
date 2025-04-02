9bcb77a62138ed82af3b1bb1111cededa65d80dd
Mikel Lorza <mikel.lorza@liferay.com>
2025-04-02 18:48:48 +0200

    LPD-49727 Implement asset subtype value

--- modules/apps/headless/headless-admin-user/headless-admin-user-impl/src/main/java/com/liferay/headless/admin/user/internal/dto/v1_0/converter/SharedAssetDTOConverter.java
+++ modules/apps/headless/headless-admin-user/headless-admin-user-impl/src/main/java/com/liferay/headless/admin/user/internal/dto/v1_0/converter/SharedAssetDTOConverter.java
@@ -7,11 +7,25 @@ package com.liferay.headless.admin.user.internal.dto.v1_0.converter;
 
 import com.liferay.headless.admin.user.dto.v1_0.SharedAsset;
 import com.liferay.headless.admin.user.internal.dto.v1_0.util.CreatorUtil;
+import com.liferay.info.exception.NoSuchInfoItemException;
+import com.liferay.info.field.InfoFieldValue;
+import com.liferay.info.item.ClassPKInfoItemIdentifier;
+import com.liferay.info.item.InfoItemServiceRegistry;
+import com.liferay.info.item.provider.InfoItemFieldValuesProvider;
+import com.liferay.info.item.provider.InfoItemObjectProvider;
+import com.liferay.info.search.InfoSearchClassMapperRegistry;
+import com.liferay.object.model.ObjectEntryFolder;
 import com.liferay.petra.function.transform.TransformUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.service.ClassNameLocalService;
 import com.liferay.portal.kernel.service.GroupLocalService;
 import com.liferay.portal.kernel.service.UserLocalService;
+import com.liferay.portal.kernel.util.File;
+import com.liferay.portal.kernel.util.MimeTypesUtil;
 import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.vulcan.dto.converter.DTOConverter;
 import com.liferay.portal.vulcan.dto.converter.DTOConverterContext;
 import com.liferay.sharing.interpreter.SharingEntryInterpreter;
@@ -61,6 +75,36 @@ public class SharedAssetDTOConverter
 						return sharingEntryInterpreter.getAssetTypeTitle(
 							sharingEntry, dtoConverterContext.getLocale());
 					});
+				setAssetSubType(
+					() -> {
+						if (StringUtil.equals(ObjectEntryFolder.class.getName(),sharingEntry.getClassName())){
+							return "folder";
+						}
+
+						Object object = _getInfoItem(sharingEntry.getClassName(),
+							sharingEntry.getClassPK());
+
+						if (object == null) {
+							return null;
+						}
+
+						InfoItemFieldValuesProvider infoItemFieldValuesProvider =
+							_getInfoItemFieldValuesProvider(
+								sharingEntry.getClassName());
+
+						if (infoItemFieldValuesProvider == null) {
+							return null;
+						}
+
+						InfoFieldValue<Object> infoFieldValue = infoItemFieldValuesProvider.getInfoFieldValue(object,"mimeType");
+
+						if (infoFieldValue!=null) {
+							return (String) infoFieldValue.getValue();
+						}
+
+						return null;
+					}
+				);
 				setClassName(sharingEntry::getClassName);
 				setClassPK(sharingEntry::getClassPK);
 				setCreator(
@@ -93,6 +137,51 @@ public class SharedAssetDTOConverter
 		};
 	}
 
+	private Object _getInfoItem(String className,long classPK)
+		throws NoSuchInfoItemException {
+
+		InfoItemObjectProvider<Object> infoItemObjectProvider =
+			_infoItemServiceRegistry.getFirstInfoItemService(
+				InfoItemObjectProvider.class, className);
+
+		if (infoItemObjectProvider == null) {
+			return null;
+		}
+
+		return infoItemObjectProvider.getInfoItem(
+			new ClassPKInfoItemIdentifier(classPK)
+		);
+
+	}
+
+	private InfoItemFieldValuesProvider<Object> _getInfoItemFieldValuesProvider(
+		String className) {
+
+		className = _infoSearchClassMapperRegistry.getClassName(className);
+
+		InfoItemFieldValuesProvider<Object> infoItemFieldValuesProvider =
+			_infoItemServiceRegistry.getFirstInfoItemService(
+				InfoItemFieldValuesProvider.class, className);
+
+		if (infoItemFieldValuesProvider == null) {
+			if (_log.isWarnEnabled()) {
+				_log.warn(
+					"Unable to get info item form provider for class " +
+					className);
+			}
+
+			return null;
+		}
+
+		return infoItemFieldValuesProvider;
+	}
+
+	@Reference
+	private InfoItemServiceRegistry _infoItemServiceRegistry;
+
+	@Reference
+	private InfoSearchClassMapperRegistry _infoSearchClassMapperRegistry;
+
 	@Reference
 	private GroupLocalService _groupLocalService;
 
@@ -105,4 +194,10 @@ public class SharedAssetDTOConverter
 	@Reference
 	private UserLocalService _userLocalService;
 
+	@Reference
+	private ClassNameLocalService _classNameLocalService;
+
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		SharedAssetDTOConverter.class);
 }
