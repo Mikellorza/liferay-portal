010a2404717ef5c87ae900eb93870bcaaec28ae9
Mikel Lorza <mikel.lorza@liferay.com>
2025-07-03 12:46:17 +0200

    LPD-58489 Add Indexable annotation for those methods that it do not have it by default in the interface or in the baseImpl

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryVersionLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryVersionLocalServiceImpl.java
@@ -23,6 +23,8 @@ import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.module.configuration.ConfigurationException;
+import com.liferay.portal.kernel.search.Indexable;
+import com.liferay.portal.kernel.search.IndexableType;
 import com.liferay.portal.kernel.security.auth.CompanyThreadLocal;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.service.UserLocalService;
@@ -164,6 +166,7 @@ public class ObjectEntryVersionLocalServiceImpl
 		objectEntryVersionPersistence.removeByObjectEntryId(objectEntryId);
 	}
 
+	@Indexable(type = IndexableType.REINDEX)
 	@Override
 	public ObjectEntryVersion expireObjectEntryVersion(
 			long userId, ObjectEntry objectEntry, int version,
@@ -176,6 +179,7 @@ public class ObjectEntryVersionLocalServiceImpl
 				objectEntry.getObjectEntryId(), version));
 	}
 
+	@Indexable(type = IndexableType.REINDEX)
 	@Override
 	public ObjectEntryVersion expireObjectEntryVersion(
 			long userId, ObjectEntryVersion objectEntryVersion)
@@ -226,6 +230,7 @@ public class ObjectEntryVersionLocalServiceImpl
 		return false;
 	}
 
+	@Indexable(type = IndexableType.REINDEX)
 	@Override
 	public ObjectEntryVersion updateLatestObjectEntryVersion(
 			ObjectEntry objectEntry)
