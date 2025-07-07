08ffaec156c27065cb1fb3baf82741d3cdfe12c6
Mikel Lorza <mikel.lorza@liferay.com>
2025-07-07 10:30:53 +0200

    LPD-58488 Improve

--- modules/apps/object/object-rest-impl/src/main/java/com/liferay/object/rest/internal/manager/v1_0/DefaultObjectEntryManagerImpl.java
+++ modules/apps/object/object-rest-impl/src/main/java/com/liferay/object/rest/internal/manager/v1_0/DefaultObjectEntryManagerImpl.java
@@ -1602,15 +1602,11 @@ public class DefaultObjectEntryManagerImpl
 			com.liferay.object.model.ObjectEntry serviceBuilderObjectEntry)
 		throws Exception {
 
-		Map<String, Map<String, String>> actions =
-			dtoConverterContext.getActions();
+		Map<String, Map<String, String>> actions = GetterUtil.getObject(
+			dtoConverterContext.getActions(), Collections::emptyMap);
 
 		if (GetterUtil.getBoolean(
 				dtoConverterContext.getAttribute("addActions"), true)) {
-
-			if (actions == null) {
-				actions = Collections.emptyMap();
-			}
 
 			HashMap<String, String> templateParameterMap = HashMapBuilder.put(
 				"version", String.valueOf(objectEntryVersion.getVersion())
@@ -2196,15 +2192,11 @@ public class DefaultObjectEntryManagerImpl
 			com.liferay.object.model.ObjectEntry serviceBuilderObjectEntry)
 		throws Exception {
 
-		Map<String, Map<String, String>> actions =
-			dtoConverterContext.getActions();
+		Map<String, Map<String, String>> actions = GetterUtil.getObject(
+			dtoConverterContext.getActions(), Collections::emptyMap);
 
 		if (GetterUtil.getBoolean(
 				dtoConverterContext.getAttribute("addActions"), true)) {
-
-			if (actions == null) {
-				actions = Collections.emptyMap();
-			}
 
 			actions = HashMapBuilder.create(
 				actions
