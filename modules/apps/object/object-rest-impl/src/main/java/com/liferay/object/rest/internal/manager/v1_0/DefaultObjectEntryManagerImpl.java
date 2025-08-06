30385f094757c6997fd70ae306a6abe3be303979
Mikel Lorza <mikel.lorza@liferay.com>
2025-08-06 09:54:15 +0200

    LPD-58489 Add Object entry version keyword contributor in order to search object entry versions by the keywords, in this case the objectDefinitionId that we are putting in the searchContext object

--- modules/apps/object/object-rest-impl/src/main/java/com/liferay/object/rest/internal/manager/v1_0/DefaultObjectEntryManagerImpl.java
+++ modules/apps/object/object-rest-impl/src/main/java/com/liferay/object/rest/internal/manager/v1_0/DefaultObjectEntryManagerImpl.java
@@ -1039,6 +1039,10 @@ public class DefaultObjectEntryManagerImpl
 
 				searchContext.setAttribute(
 					"entryClassName", ObjectEntryVersion.class.getName());
+
+				searchContext.setAttribute(
+					"objectDefinitionId",
+					serviceBuilderObjectEntry.getObjectDefinitionId());
 			},
 			sorts,
 			document -> _objectEntryDTOConverter.toDTO(
