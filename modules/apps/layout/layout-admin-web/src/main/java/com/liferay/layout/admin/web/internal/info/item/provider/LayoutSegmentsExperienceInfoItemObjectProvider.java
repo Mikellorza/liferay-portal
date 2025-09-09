ab4899a510857fc180e114f920a49bf4649c52b5
Mikel Lorza <mikel.lorza@liferay.com>
2025-09-09 12:29:11 +0200

    LPD-64972 Add support for ERCInfoItemIdentifier in LayoutSegmentsExperienceInfoItemDetailsProvider

--- modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/info/item/provider/LayoutSegmentsExperienceInfoItemObjectProvider.java
+++ modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/info/item/provider/LayoutSegmentsExperienceInfoItemObjectProvider.java
@@ -7,9 +7,17 @@ package com.liferay.layout.admin.web.internal.info.item.provider;
 
 import com.liferay.info.exception.NoSuchInfoItemException;
 import com.liferay.info.item.ClassPKInfoItemIdentifier;
+import com.liferay.info.item.ERCInfoItemIdentifier;
 import com.liferay.info.item.InfoItemIdentifier;
 import com.liferay.info.item.provider.InfoItemObjectProvider;
 import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.security.auth.CompanyThreadLocal;
+import com.liferay.portal.kernel.service.GroupLocalService;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.ServiceContextThreadLocal;
+import com.liferay.portal.kernel.util.GroupThreadLocal;
+import com.liferay.portal.kernel.util.Validator;
 import com.liferay.segments.model.SegmentsExperience;
 import com.liferay.segments.service.SegmentsExperienceLocalService;
 
@@ -20,7 +28,10 @@ import org.osgi.service.component.annotations.Reference;
  * @author Adolfo Pérez
  */
 @Component(
-	property = "info.item.identifier=com.liferay.info.item.ClassPKInfoItemIdentifier",
+	property = {
+		"info.item.identifier=com.liferay.info.item.ClassPKInfoItemIdentifier",
+		"info.item.identifier=com.liferay.info.item.ERCInfoItemIdentifier"
+	},
 	service = InfoItemObjectProvider.class
 )
 public class LayoutSegmentsExperienceInfoItemObjectProvider
@@ -30,25 +41,105 @@ public class LayoutSegmentsExperienceInfoItemObjectProvider
 	public SegmentsExperience getInfoItem(InfoItemIdentifier infoItemIdentifier)
 		throws NoSuchInfoItemException {
 
-		if (!(infoItemIdentifier instanceof ClassPKInfoItemIdentifier)) {
+		return getInfoItem(_getGroupId(), infoItemIdentifier);
+	}
+
+	@Override
+	public SegmentsExperience getInfoItem(
+			long groupId, InfoItemIdentifier infoItemIdentifier)
+		throws NoSuchInfoItemException {
+
+		if (!(infoItemIdentifier instanceof ClassPKInfoItemIdentifier) &&
+			!(infoItemIdentifier instanceof ERCInfoItemIdentifier)) {
+
 			throw new NoSuchInfoItemException(
 				"Unsupported info item identifier " + infoItemIdentifier);
 		}
 
-		ClassPKInfoItemIdentifier classPKInfoItemIdentifier =
-			(ClassPKInfoItemIdentifier)infoItemIdentifier;
-
 		try {
-			return _segmentsExperienceLocalService.getSegmentsExperience(
-				classPKInfoItemIdentifier.getClassPK());
+			if (infoItemIdentifier instanceof ClassPKInfoItemIdentifier) {
+				ClassPKInfoItemIdentifier classPKInfoItemIdentifier =
+					(ClassPKInfoItemIdentifier)infoItemIdentifier;
+
+				return _segmentsExperienceLocalService.getSegmentsExperience(
+					classPKInfoItemIdentifier.getClassPK());
+			}
+
+			ERCInfoItemIdentifier ercInfoItemIdentifier =
+				(ERCInfoItemIdentifier)infoItemIdentifier;
+
+			return _segmentsExperienceLocalService.
+				getSegmentsExperienceByExternalReferenceCode(
+					ercInfoItemIdentifier.getExternalReferenceCode(),
+					_getGroupId(groupId, ercInfoItemIdentifier));
 		}
 		catch (PortalException portalException) {
-			throw new NoSuchInfoItemException(
-				"No segments experience found with ID " +
-					classPKInfoItemIdentifier.getClass(),
-				portalException);
+			throw new NoSuchInfoItemException(portalException);
 		}
 	}
+
+	private long _getCompanyId() {
+		ServiceContext serviceContext =
+			ServiceContextThreadLocal.getServiceContext();
+
+		if (serviceContext != null) {
+			return serviceContext.getCompanyId();
+		}
+
+		Long companyId = CompanyThreadLocal.getCompanyId();
+
+		if (companyId != null) {
+			return companyId;
+		}
+
+		throw new IllegalStateException(
+			"Neither service context thread local nor company thread local " +
+				"are initialized");
+	}
+
+	private long _getGroupId() {
+		ServiceContext serviceContext =
+			ServiceContextThreadLocal.getServiceContext();
+
+		if (serviceContext != null) {
+			return serviceContext.getScopeGroupId();
+		}
+
+		Long groupId = GroupThreadLocal.getGroupId();
+
+		if (groupId != null) {
+			return groupId;
+		}
+
+		throw new IllegalStateException(
+			"Neither service context thread local nor group thread local are " +
+				"initialized");
+	}
+
+	private long _getGroupId(
+			long groupId, ERCInfoItemIdentifier ercInfoItemIdentifier)
+		throws NoSuchInfoItemException {
+
+		try {
+			if (Validator.isNull(
+					ercInfoItemIdentifier.getScopeExternalReferenceCode())) {
+
+				return groupId;
+			}
+
+			Group group = _groupLocalService.getGroupByExternalReferenceCode(
+				ercInfoItemIdentifier.getScopeExternalReferenceCode(),
+				_getCompanyId());
+
+			return group.getGroupId();
+		}
+		catch (PortalException portalException) {
+			throw new NoSuchInfoItemException(portalException);
+		}
+	}
+
+	@Reference
+	private GroupLocalService _groupLocalService;
 
 	@Reference
 	private SegmentsExperienceLocalService _segmentsExperienceLocalService;
