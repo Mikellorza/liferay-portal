9e552964f5016025a63605fb5bcf189c080f9048
Rubén Pulido <ruben.pulido@liferay.com>
2025-10-24 15:03:32 +0200

    LPD-61268 Add helper method to create a Page Element of type Collection Item

--- modules/apps/headless/headless-admin-site/headless-admin-site-test/src/testIntegration/java/com/liferay/headless/admin/site/resource/v1_0/test/PageElementResourceTest.java
+++ modules/apps/headless/headless-admin-site/headless-admin-site-test/src/testIntegration/java/com/liferay/headless/admin/site/resource/v1_0/test/PageElementResourceTest.java
@@ -18,6 +18,7 @@ import com.liferay.headless.admin.site.client.dto.v1_0.CollectionDisplayPageElem
 import com.liferay.headless.admin.site.client.dto.v1_0.CollectionDisplayViewport;
 import com.liferay.headless.admin.site.client.dto.v1_0.CollectionDisplayViewportDefinition;
 import com.liferay.headless.admin.site.client.dto.v1_0.CollectionItemExternalReference;
+import com.liferay.headless.admin.site.client.dto.v1_0.CollectionItemPageElementDefinition;
 import com.liferay.headless.admin.site.client.dto.v1_0.CollectionReference;
 import com.liferay.headless.admin.site.client.dto.v1_0.CollectionSettings;
 import com.liferay.headless.admin.site.client.dto.v1_0.ContainerPageElementDefinition;
@@ -989,6 +990,29 @@ public class PageElementResourceTest extends BasePageElementResourceTestCase {
 				}
 			}
 		};
+	}
+
+	private PageElement _getCollectionItemPageElement(
+			String pageElementExternalReferenceCode,
+			String parentPageElementExternalReferenceCode,
+			PageElement[] pageElements)
+		throws Exception {
+
+		CollectionItemPageElementDefinition
+			collectionItemPageElementDefinition =
+				new CollectionItemPageElementDefinition();
+
+		collectionItemPageElementDefinition.setType(
+			PageElementDefinition.Type.COLLECTION_ITEM);
+
+		PageElement pageElement = _getPageElement(
+			collectionItemPageElementDefinition,
+			pageElementExternalReferenceCode, pageElements);
+
+		pageElement.setParentExternalReferenceCode(
+			parentPageElementExternalReferenceCode);
+
+		return pageElement;
 	}
 
 	private CollectionReference _getCollectionReference(
