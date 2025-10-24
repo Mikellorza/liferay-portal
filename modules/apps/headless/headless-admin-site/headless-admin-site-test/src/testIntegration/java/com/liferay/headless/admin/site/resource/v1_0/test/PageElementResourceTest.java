b812189f3c14dbcac3d523840368f0ee644b310c
Rubén Pulido <ruben.pulido@liferay.com>
2025-10-24 15:37:52 +0200

    LPD-61268 Don't compare child page elements of grid PageElements for now

--- modules/apps/headless/headless-admin-site/headless-admin-site-test/src/testIntegration/java/com/liferay/headless/admin/site/resource/v1_0/test/PageElementResourceTest.java
+++ modules/apps/headless/headless-admin-site/headless-admin-site-test/src/testIntegration/java/com/liferay/headless/admin/site/resource/v1_0/test/PageElementResourceTest.java
@@ -596,8 +596,10 @@ public class PageElementResourceTest extends BasePageElementResourceTestCase {
 
 		Assert.assertNotNull(pageElementDefinition);
 
-		if (pageElementDefinition.getType() ==
-				PageElementDefinition.Type.COLLECTION_DISPLAY) {
+		if ((pageElementDefinition.getType() ==
+				PageElementDefinition.Type.COLLECTION_DISPLAY) ||
+			(pageElementDefinition.getType() ==
+				PageElementDefinition.Type.GRID)) {
 
 			return;
 		}
